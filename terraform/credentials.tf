cae813c9d150c667b3415527a253432489086201
Bernardo Llamas Verna <bernardo.llamas@sngular.com>
2022-03-08 21:26:51 +0100

    primeros pasos

---
+++ terraform/credentials.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "77c88c55-f1f1-487b-91fd-8d3122bf6a57"
+  client_id       = "66140cd9-607e-4d24-84db-92061d00ffde"
+  client_secret   = "2uMylDduNa9z.Kf2g2MTojv9BV9d~x8_1g"
+  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
+}
